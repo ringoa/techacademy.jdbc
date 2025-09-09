27a2d8892db2fd2cb2b777cdc93cbd7fc07e9814
ringoa <84253928+ringoa@users.noreply.github.com>
2025-09-09 20:55:09 +0100

    "09.Javaからpersonテーブルのデータを取得しよう（JDBC1）"の課題

---
+++ src/dbSample/review06.java
@@ -0,0 +1,93 @@
+package dbSample;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class review06 {
+
+	public static void main(String[] args) {
+
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			con = DriverManager.getConnection(
+					"jdbc:mysql://localhost/kadaidb?useSSL=false&allowPublicKeyRetrieval=true",
+					"root",
+					"K9m!z4#Qw8Rp2@Ln"
+					);
+
+			String sql = "SELECT * FROM person WHERE id = ?";
+
+			pstmt = con.prepareStatement(sql);
+
+			System.out.println("idを入力してください>");
+			int input_id = Integer.parseInt(keyIn());
+
+			pstmt.setInt(1, input_id);
+
+			rs = pstmt.executeQuery();
+
+			while(rs.next()) {
+				String name = rs.getString("name");
+				int age = rs.getInt("age");
+
+				System.out.println(name);
+				System.out.println(age);
+			}
+
+
+		} catch (ClassNotFoundException e) {
+			System.err.println("JDBCドライバのロードに失敗しました。");
+			e.printStackTrace();
+		} catch (SQLException e) {
+			System.err.println("データベースに異常が発生しました。");
+			e.printStackTrace();
+		} finally {
+
+			if(rs != null) {
+				try {
+					rs.close();
+				} catch(SQLException e) {
+					System.err.println("ResultSetを閉じるときにエラーが発生しました。");
+                    e.printStackTrace();
+				}
+			}
+			if(pstmt != null) {
+				try {
+					pstmt.close();
+				} catch(SQLException e) {
+					System.err.println("Statementを閉じるときにエラーが発生しました。");
+                    e.printStackTrace();
+				}
+			}
+			if(con != null) {
+				try {
+					con.close();
+				} catch (SQLException e) {
+                    System.err.println("データベース切断時にエラーが発生しました。");
+                    e.printStackTrace();
+				}
+			}
+		}
+	}
+	private static String keyIn() {
+		String line = null;
+		try {
+			BufferedReader key = new BufferedReader(new InputStreamReader(System.in));
+			line = key.readLine();
+		}	catch (IOException e){
+			System.err.println("入力の読み取り中にエラーが発生しました。");
+	        e.printStackTrace();
+		}
+		return line;
+	}
+}
